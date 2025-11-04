d1da9b1528b38e1091678a09c904bdaa3e49aa53
Darius Kiptoo <dariusngetich025.gmail.com>
2025-11-04 12:51:15 +0300

    initial commit

---
+++ basic_operations.py
@@ -0,0 +1,10 @@
+number1 = 10
+number2 =5
+
+sumoftwo = (number1 + number2)
+substraction = (number1 - number2)
+multiply = (number1 *number2)
+
+print("Addition of 10 and 5 is", sumoftwo)
+print ("Substraction of 10 and 5 is", substraction)
+print ("Multiplication of 10 and 5 is", multiply)
---
+++ future_age_calculator.py
@@ -0,0 +1,5 @@
+userage = int(input("How old are you?" ))
+infifty = userage + 27
+
+print("In 2050, you will be",infifty,"years old" )
+
