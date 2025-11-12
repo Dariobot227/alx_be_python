c05d707e1d416ee038ce940146c3d11ffbcca1f5
Darius Kiptoo <dariusngetich025.gmail.com>
2025-11-12 14:54:35 +0300

    final waether update

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,10 +1,10 @@
-quiz = input("What's the weather like today? (sunny/rainy/cold): ").lower()
+weather = input("What's the weather like today? (sunny/rainy/cold): ").lower()
 
-if quiz == "sunny":
+if weather == "sunny":
     print("Wear a t-shirt and sunglasses.")
-elif quiz == "rainy":
+elif weather == "rainy":
     print(" Don't forget your umbrella and a raincoat.")
-elif quiz == "cold":
+elif weather == "cold":
     print("Make sure to wear a warm coat and a scarf.")
 else:
     print("Sorry, I don't have recommendations for this weather.")
